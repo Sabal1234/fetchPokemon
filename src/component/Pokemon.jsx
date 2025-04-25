735ccb704e144eb3de2425818a679bb173659342
Sabal Adhikari <sadaladhikari@gmail.com>
2025-04-25 15:53:58 +0545

    update

--- src/component/Pokemon.jsx
+++ src/component/Pokemon.jsx
@@ -3,49 +3,38 @@ import { useQuery } from '@tanstack/react-query';
 
 export const Pokemon = () => {
   const [pokemonName, setPokemonName] = useState('');
-  const [search, setSearch] = useState('');
 
   const { data, isLoading, isError } = useQuery({
-    queryKey: ['pokemon', search],
+    queryKey: ['pokemon', pokemonName],
     queryFn: () =>
-      fetch(`https://pokeapi.co/api/v2/pokemon/${search}`)
+      fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName.toLowerCase()}`)
         .then((res) => res.json()),
-    enabled: !!search, 
+    enabled: !!pokemonName.trim(), 
   });
 
-  const handleSearch = () => {
-    setSearch(pokemonName); 
-  };
-
-  if (isError) {
-    return <h1>Sorry, there is an error</h1>;
-  }
-
-  if (isLoading) {
-    return <h1>Loading...</h1>;
-  }
-return (
-  <div className="pokemon-container">
-    <h2>Search Pokemon</h2>
-    <div>
+  return (
+    <div className="pokemon-container">
+      <h2>Search Pokemon</h2>
       <input
         type="text"
         value={pokemonName}
         onChange={(e) => setPokemonName(e.target.value)}
       />
-      <button onClick={handleSearch}>Search</button>
+
+      {isError && <h1>Sorry, there is an error</h1>}
+      {isLoading && <h1>Loading...</h1>}
+
+      {data && !data.detail && (
+        <div>
+          <h3>{data.name}</h3>
+          <img src={data.sprites.front_default} alt={data.name} width="150" />
+          <p>Height: {data.height}</p>
+          <p>Weight: {data.weight}</p>
+          <p>Types: {data.types.map((type) => type.type.name)}</p>
+        </div>
+      )}
+
+      {data?.detail && <p>Pokemon not found.</p>}
     </div>
-
-    {data && (
-      <div>
-        <h3>{data.name}</h3>
-        <img src={data.sprites.front_default} alt={data.name} width="150" />
-        <p>Height: {data.height}</p>
-        <p>Weight: {data.weight}</p>
-        <p>Types: {data.types.map((type) => type.type.name).join(', ')}</p>
-      </div>
-    )}
-  </div>
-);
-
+  );
 };
